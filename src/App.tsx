dd3703c3a857ecdf72147e3e417899133358182e
Raniel Mendonça <raniel.mendonca@zup.com.br>
2022-07-31 03:06:11 -0300

    feat: adding first page

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,18 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
+import "./App.css";
 
 const App = () => {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="App">
       <h1>Welcome to React Router!</h1>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-    </>
+    </div>
   );
 };
 
